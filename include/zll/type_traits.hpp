8f2c1b9ded35695ae442ef51e6d8be150b33cba3
Tobias Zindl <zie.develop@gmail.com>
2023-10-06 21:23:03 +0200

    :sparkles: add is_floating_point trait

--- include/zll/type_traits.hpp
+++ include/zll/type_traits.hpp
@@ -11,6 +11,7 @@
 
 #include "zll/meta/is_array.hpp"
 #include "zll/meta/is_class.hpp"
+#include "zll/meta/is_floating_point.hpp"
 #include "zll/meta/is_integral.hpp"
 #include "zll/meta/is_member_pointer.hpp"
 #include "zll/meta/is_pointer.hpp"
---
+++ tests/unit_tests/meta/is_floating_point_tests.cpp
@@ -0,0 +1,24 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+#include "zll/meta/is_floating_point.hpp"
+
+#include <utest.h>
+
+class test_class {};
+
+UTEST(meta_is_floating_point, is_floating_point) {
+    ASSERT_TRUE(zll::meta::is_floating_point<float>::value);
+    ASSERT_TRUE(zll::meta::is_floating_point<const double>::value);
+    ASSERT_TRUE(zll::meta::is_floating_point<volatile float>::value);
+    ASSERT_TRUE(zll::meta::is_floating_point<const volatile double>::value);
+    
+    ASSERT_FALSE(zll::meta::is_floating_point<test_class>::value);
+    ASSERT_FALSE(zll::meta::is_floating_point<float&>::value);
+    ASSERT_FALSE(zll::meta::is_floating_point<double*>::value);
+}
+
+
