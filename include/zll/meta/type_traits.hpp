8680f973f87bf4aa829d8dd839016e7d7837bc47
Tobias Zindl <zie.develop@gmail.com>
2023-10-13 18:52:30 +0200

    :sparkles: add remove_reference trait

--- include/zll/meta/type_traits.hpp
+++ include/zll/meta/type_traits.hpp
@@ -15,15 +15,18 @@
 #include "zll/meta/traits/is_const.hpp"
 #include "zll/meta/traits/is_enum.hpp"
 #include "zll/meta/traits/is_floating_point.hpp"
+#include "zll/meta/traits/is_function.hpp"
 #include "zll/meta/traits/is_integral.hpp"
 #include "zll/meta/traits/is_member_pointer.hpp"
 #include "zll/meta/traits/is_object.hpp"
 #include "zll/meta/traits/is_pointer.hpp"
+#include "zll/meta/traits/is_reference.hpp"
 #include "zll/meta/traits/is_same.hpp"
 #include "zll/meta/traits/is_scalar.hpp"
 #include "zll/meta/traits/is_union.hpp"
 
 #include "zll/meta/traits/remove_cv.hpp"
+#include "zll/meta/traits/remove_reference.hpp"
 
 
 #endif // ZLL_TYPETRAITS_HPP
